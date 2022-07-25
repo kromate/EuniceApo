61df785c056b8a610f0e6b56c007fee3233d2cdf
kromate <akpananthony33@gmail.com>
2022-07-25 02:40:39 +0100

    feat: intial setup and shit

---
+++ src/views/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<DefaultLayout>
+		<h1 class="text-7xl font-bold">Eunice Apo</h1>
+	</DefaultLayout>
+</template>
+
+<script setup lang="ts">
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
+</script>
+
+<style scoped></style>
